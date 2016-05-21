d332bc8ce2eae60dca8dad5271c0815d30125340
dipudangol <satandangol1@gmail.com>
2016-05-21 20:38:22 +0545

    This is parser advanced

--- parser/src/main/java/np/com/rts/internship/beginners/Main.java
+++ parser/src/main/java/np/com/rts/internship/beginners/Main.java
@@ -14,8 +14,23 @@ public class Main {
 
   public Main() {
     // Write your code here
+      ArrayList<Integer> arr=new ArrayList<Integer>();
+      ArrayList<String> str=new ArrayList<String>();
 
+      //System.out.println(parseThis);
+    String ola[]=parseThis.split("\n");
+      String second=ola[1];
 
+      char[] ascii1 = second.toCharArray();
+      for(char ch:ascii1){
+          arr.add((int)ch);
+      }
+      for(int i:arr){
+          str.add(Character.toString ((char) (i-1)));
+      }
+      String yo=str.get(6)+str.get(7)+str.get(8);
+      double val = Double.parseDouble(yo);
+      value=val;
   }
 
   public static void main(String[] args) {
